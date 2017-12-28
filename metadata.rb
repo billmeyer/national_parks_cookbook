294e2ad651a2ca8c5b53b52a57041b4c20f73960
Bill Meyer <stinkjuice@gmail.com>
2017-12-28 12:33:55 -0600

    Fixed the dashes in the recipe names

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bill@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures national_parks'
 long_description 'Installs/Configures national_parks'
-version '0.1.17'
+version '0.1.18'
 chef_version '>= 12.5' if respond_to?(:chef_version)
 
 supports 'centos'
